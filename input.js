06d7bd367ac6e87b8bf25edb42b672872a38e0c2
ari29112003 <arielleguterres@gmail.com>
2023-08-28 15:20:34 -0300

    input

--- input.js
+++ input.js
@@ -1,4 +1,14 @@
-const setupInput = function () {
+const net = require("net");
+let connection;
+
+const setupInput = function (conn) {
+  conn = net.createConnection(
+    {
+      host: "localhost", // IP address here,
+      port: 50541  // PORT number here,
+    });
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,17 +16,39 @@ const setupInput = function () {
   stdin.on('data', handleUserInput);
   return stdin;
 }
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     console.log(`    exit game   `);
     process.exit()
+    //if crtl+c pressed exit game
 
   } if (key === 's') {
-    process.stdout.write(`Move: down`)
+    connection.write(`Move: down`);
+    // "Move: down" - move down one square
 
-  }
+  } if (key === 'w') {
+    connection.write("Move: up");
+    //"Move: up" - move up one square
+
+  } if (key === 'a') {
+    connection.write("Move: left");
+    // "Move: left" - move left one square
+
+  } if (key === 'd') {
+    connection.write("Move: right");
+    //"Move: rigth" - move rigth one square
+  
+  } if (key=== 'k'){
+    connection.write("Say: kkkk ")
+  
+  } if (key=== 'n'){
+    connection.write("Say: nice")
+   
+  } if (key === 'b'){
+    connection.write("Say: beijoss")
+  } 
 };
-
 
 setupInput()
 
@@ -24,4 +56,5 @@ setupInput()
 module.exports = {
   setupInput
 }
+
 
