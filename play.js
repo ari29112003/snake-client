90f7bf7affd96b3aa2bf49bb4bc4dbd7ededee59
ari29112003 <arielleguterres@gmail.com>
2023-08-28 11:54:47 -0300

    changes

--- play.js
+++ play.js
@@ -1,2 +1,26 @@
 const play = require("./client");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+ return stdin;
+}
+  const handleUserInput = function (key) {
+      if (key === '\u0003') {
+        console.log (`    exit game   `);
+      process.exit()
+
+      } if (key === 's') {
+       process.stdout.write(`Move: down`)
+
+      }
+    };
+  
+
+setupInput()
+
