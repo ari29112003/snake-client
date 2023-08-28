393cd139834791bd7a9c9d7268cec1ba36c4f358
ari29112003 <arielleguterres@gmail.com>
2023-08-28 11:55:23 -0300

    changes

--- client.js
+++ client.js
@@ -3,22 +3,19 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection(
-    {host:"localhost", // IP address here,
-    port:  50541  // PORT number here,
-});
-conn.on("connect",()=>{
-  console.log(`Name:AFG`);
-})
-//to logged the message when you make the conection 
-conn.on("connect", () => {
-  console.log("successfully connected!");
-}).on("error", (err) => {
-  console.log(err);
-});
-  
+    {
+      host: "localhost", // IP address here,
+      port: 50541  // PORT number here,
+    });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
+  // logge a message when you make the conection 
+  conn.on("connect", () => {
+    console.log(`Name:AFG`);
+    console.log("successfully connected!");
+  });
 
   return conn;
 };
@@ -26,7 +23,7 @@ conn.on("connect", () => {
 console.log("Connecting ...");
 connect();
 
-module.exports ={
-  host:"localhost",
-  port:50541
+module.exports = {
+  host: "localhost",
+  port: 50541
 }
